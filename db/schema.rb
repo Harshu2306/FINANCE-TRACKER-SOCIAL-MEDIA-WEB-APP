f28f08d81d4780aa12fce56bfebb163d1d2e5860
Harshu2306 <harshpatelvi.dev@gmail.com>
2022-03-22 18:20:33 +0530

    stock model and hirb&irb-ruby-client gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_075752) do
+ActiveRecord::Schema.define(version: 2022_03_22_121320) do
+
+  create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price", precision: 10
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
