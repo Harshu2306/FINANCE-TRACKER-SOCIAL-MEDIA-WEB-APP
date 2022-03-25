a55be98c90a3dbc7e8a38333cbf6a42b62f242ce
Harshu2306 <harshpatelvi.dev@gmail.com>
2022-03-25 20:09:27 +0530

    added friend model in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,46 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks
 
   end
+
+  def my_friends
+
+    @friends = current_user.friends
+
+  end
+
+  def search
+
+    if params[:friend].present?
+      @friend = Stock.new_lookup(params[:friend])
+
+      if @friend
+        respond_to do |format|
+          format.js{ render partial: 'users/friend_result'}
+
+        end
+
+
+      else
+        respond_to do |format|
+          flash.now[:alert]="Please Enter a valid friend name to Search"
+            format.js{ render partial: 'users/friend_result'}
+
+          end
+
+      end
+
+
+    else
+      respond_to do |format|
+          flash.now[:alert]="Please Enter a friend's name to Search"
+            format.js{ render partial: 'users/friend_result'}
+          end
+
+
+    end
+
+
+
+  end
   
 end
